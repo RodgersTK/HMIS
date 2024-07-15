698b64a5bc04a7202d3aadbdb7055725fb589447
RodgersTK <rodgerstkm@gmail.com>
2024-07-15 11:19:19 +0300

    final_changes

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1 className="text-center ml-6 text-3xl font-bold mt-6">My Hosi App</h1>
+    </div>
+  );
+}
+
+export default Header;
